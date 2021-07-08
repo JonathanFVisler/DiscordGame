c8e9beb19400098628de245ade674951f5e15cdb
Zenobe <levendeslot@gmail.com>
2021-07-08 22:28:23 +0200

    basic database setup

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,28 @@ use serenity::{
     framework::standard::{CommandResult, StandardFramework},
 
 };
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Mutex;
+use rusqlite::{Connection};
 use dotenv::var;
 
 const PREFIX: &str = "+";
 
-#[derive(Default)]
-struct Global {
-    count: AtomicUsize,
-}
+struct DB;
 
-struct GlobalKey;
-
-impl TypeMapKey for GlobalKey {
-    type Value = Global;
+impl TypeMapKey for DB {
+    type Value = Mutex<Connection>;
 }
 
 #[group]
-#[commands(ping, please_say, increase)]
+#[commands(ping, please_say)]
 struct General;
 
 #[tokio::main]
 async fn main() {
     let token = var("DISCORD_TOKEN").unwrap();
+    let db_path = var("DATABASE_PATH").unwrap();
+
+    let db = Connection::open(db_path).unwrap();
 
     let framework = StandardFramework::new()
         .configure(|c| c.prefix(PREFIX))
@@ -37,7 +36,7 @@ async fn main() {
 
     {
         let mut data = client.data.write().await;
-        data.insert::<GlobalKey>(Global::default());
+        data.insert::<DB>(Mutex::new(db));
     }
 
     client.start().await.unwrap()
@@ -55,13 +54,3 @@ async fn please_say(ctx: &Context, msg: &Message) -> CommandResult {
     msg.channel_id.say(&ctx.http, content.trim_start()).await?;
     Ok(())
 }
-
-#[command]
-async fn increase(ctx: &Context, msg: &Message) -> CommandResult {
-    let data = ctx.data.read().await;
-    let global = data.get::<GlobalKey>().unwrap();
-    let val = global.count.fetch_add(1, Ordering::SeqCst) + 1;
-    msg.channel_id.say(&ctx.http, format!("count: {}", val)).await?;
-    Ok(())
-}
-
